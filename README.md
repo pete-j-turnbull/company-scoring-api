4456c76d8108b46da512e9544017e8e7530743f1
Pete Turnbull <pete.j.turnbull@gmail.com>
2016-06-29 10:35:03 +0100

    Added officer information to scoring algo

---
+++ README.md
@@ -0,0 +1,10 @@
+# company-scoring-api
+
+## Setup
+ - docker build . -t company-scoring-api
+ - docker run -p 8888:5000 -dcompany-scoring-api
+
+Access the api at http://localhost:8888/scoreCompany?companyNumber={companyNumber}
+
+Live version at http://whimz.co/scoreCompany
+
--- src/routes.js
+++ src/routes.js
@@ -7,102 +7,115 @@ var config = require('./config/config');
 var objectAssign = require('object-assign');
 
 
+var _normalize = function (weights) {
+	var sumWeights = _(weights).sum();
+	var normalizer = 10 / sumWeights;
+
+	var nWeights = _(weights)
+		.map(function (weight) {
+			return weight * normalizer
+		})
+		.value();
+	return nWeights;
+};
 var _score = function (companyProfile) {
 	log.info(companyProfile);
-	var weights = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+	var weights = [0.5, 1.5, 2, 1.5, 3, 3, 2, 0.5, 0.5];
+
+	//normalize
+	var nWeights = _normalize(weights)
 
 	return 10 - (
-		weights[0] * companyProfile.undeliverable_registered_office_address +
-		weights[1] * companyProfile.accounts.overdue +
-		weights[2] * companyProfile.has_been_liquidated +
-		weights[3] * companyProfile.annual_return.overdue +
-		weights[4] * companyProfile.has_insolvency_history +
-		weights[5] * (companyProfile.status == 'active') +
-		weights[6] * companyProfile.has_insolvency_history +
-		weights[7] * companyProfile.has_charges +
-		weights[8] * companyProfile.can_file );
-
-	//weights[0] * companyProfile.type +
-	//TODO: incorp officers information
-
-
-	return 10;
+		weights[0] * companyProfile.undeliverableRegisteredOfficeAddress +
+		weights[1] * companyProfile.accountsOverdue +
+		weights[2] * companyProfile.hasBeenLiquidated +
+		weights[3] * companyProfile.annualReturnOverdue +
+		weights[4] * companyProfile.inActive +
+		weights[5] * companyProfile.hasInsolvencyHistory +
+		weights[6] * companyProfile.hasCharges +
+		weights[7] * !companyProfile.canFile +
+		weights[8] * (companyProfile.resignedOfficerCount / companyProfile.activeOfficerCount)); // Probably not important but perhaps lots of officers resigning at once could be a red flag
 }
 
-module.exports.scoreCompany = function *() {
-	var companyNumber = this.query.company_number;
-	var companyName = this.query.company_name;
 
-	var baseOptions = {
-		headers: {
-			'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36'
-		},
-		auth: {
-			'user': config.chApiKey
-		}
+var getOfficers = function *(baseOptions, companyNumber) {
+	var opts = objectAssign({}, baseOptions, { url: 'https://api.companieshouse.gov.uk/company/' + companyNumber + '/officers' });
+	var officersResp = yield request(opts);
+	var officers = JSON.parse(officersResp.body);
+	return officers;
+};
+var getCompanyInfo = function *(baseOptions, companyNumber) {
+	var opts = objectAssign({}, baseOptions, { url: 'https://api.companieshouse.gov.uk/company/' + companyNumber });
+	var companyInfoResp = yield request(opts);
+	var companyInfo = JSON.parse(companyInfoResp.body);
+	return companyInfo;
+};
+var buildCompanyProfile = function (companyInfo, officers) {
+	var companyProfile = {
+		undeliverableRegisteredOfficeAddress: companyInfo.undeliverable_registered_office_address,
+		accountsOverdue: companyInfo.accounts.overdue,
+		hasBeenLiquidated: companyInfo.has_been_liquidated,
+		annualReturnOverdue: companyInfo.annual_return.overdue,
+		inActive: companyInfo.status != 'active',
+		hasInsolvencyHistory: companyInfo.has_insolvency_history,
+		hasCharges: companyInfo.has_charges,
+		canFile: companyInfo.can_file,
+		resignedOfficerCount: officers.resigned_count,
+		activeOfficerCount: officers.active_count
 	};
+	return companyProfile;
+};
 
-	if (companyNumber) {
-		// We have a companyNumber
-		var opts = objectAssign({}, baseOptions, { url: 'https://api.companieshouse.gov.uk/company/' + companyNumber });
-		var companyResp = yield request(opts);
-		var companyProfile = JSON.parse(companyResp.body);
+var getCompanyNumber = function *(baseOptions, companyName) {
+	var opts = objectAssign({}, baseOptions, { url: 'https://api.companieshouse.gov.uk/search/companies/?q=' + companyName });
+	var searchResp = yield request(opts);
+	var searchResults = JSON.parse(searchResp.body);
 
-		// Now score the company with the data at hand. TODO
+	if (searchResults.total_results > 0) {
+		var companyNumber = searchResults.items[0].company_number;
+		return companyNumber;
+	} else {
+		throw new Error('No companies found with that name.');
+	}
+};
+
+
+module.exports.scoreCompany = function *() {
+	try {
+		var companyNumber = this.query.companyNumber;
+		var companyName = this.query.companyName;
+
+		if (!companyNumber && !companyName) throw new Error('No company_number or company_name supplied.');
+
+		var baseOptions = {
+			headers: {
+				'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36'
+			},
+			auth: {
+				'user': config.chApiKey
+			}
+		};
+
+		if (!companyNumber) companyNumber = yield getCompanyNumber(companyName);
+
+		// Now we have a company number
+
+		var companyInfo = yield getCompanyInfo(baseOptions, companyNumber);
+		var officers = yield getOfficers(baseOptions, companyNumber); // These queries need to be run in parallel with Promise.all
+
+		var companyProfile = buildCompanyProfile(companyInfo, officers);
 		var score = _score(companyProfile);
-		var response = { success: true, result: { companyNumber: companyProfile.company_number, score: score }};
+
+		var response = { success: true, result: { companyNumber: companyNumber, score: score }};
 		this.body = response;
 
-	} else if (companyName) {
-		// We have a company name to search on
-		var opts = objectAssign({}, baseOptions, { url: 'https://api.companieshouse.gov.uk/search/companies/?q=' + companyName });
-		var searchResp = yield request(opts);
-		var searchResults = JSON.parse(searchResp.body);
 
-		// For now choose the first company in search results
-		if (searchResults.total_results > 0) {
-			var companyProfile = searchResults.items[0]
-			// Now score the company with the data at hand. TODO
-			var score = _score(companyProfile);
-			var response = { success: true, result: { companyNumber: companyProfile.company_number, score: score }};
-			this.body = response;
 
-		} else {
-			var response = { success: false, result: 'No companies found with that name.' };
-			this.body = response;
-		}
-
-	} else {
-		var response = { success: false, result: 'No company_number or company_name supplied.' };
+	} catch (err) {
+		var response = { success: false, result: String(err) };
 		this.body = response;
 	}
-
-
-	/*
-	var options = {
-		url: 'https://api.companieshouse.gov.uk/company/' + '04647910',
-		headers: {
-			'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36'
-		},
-		auth: {
-			'user': 'ZsgCqaqCZSMA7CZK3ovuNrEqy06ktpcX5P_WLP5g'
-		}
-	};
-
-	var options = {
-		url: 'https://api.companieshouse.gov.uk/search/companies?q=' + 'anyclean',
-		headers: {
-			'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36'
-		},
-		auth: {
-			'user': 'ZsgCqaqCZSMA7CZK3ovuNrEqy06ktpcX5P_WLP5g'
-		}
-	};
-
-	var resp = yield request(options);
-	var response = { success: true, result: resp.body };
-	this.body = response;
-	*/
 }
 
 	
